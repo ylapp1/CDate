89b0d2f2aae14174d44189898d9324255d33e7a6
Yannick Lapp <yannick.lapp@cn-consult.eu>
2019-10-31 12:07:58 +0100

    Fix docs script

--- docs/generate-docs.sh
+++ docs/generate-docs.sh
@@ -1,26 +1,53 @@
 #!/bin/bash
 
-# Check if the current working directory contains the latex document
-if [ ! -f "Dokumentation.tex" ]; then
-    echo "Please execute from inside the docs directory"
-    exit 1
-fi
+#
+# Returns an absolute path from a path string.
+#
+# @param string $1 The path string
+#
+getAbsolutePath()
+{
+    pathString="$1"
+
+    if [[ "$pathString" == "/"* ]]; then
+
+        # If the path starts with a slash it is already an absolute path
+        absolutePath=$pathString
+    else
+        # If the path does not start with a slash it is a relative path
+        # Therefore the current working directory (absolute path) is added in front of it
+        absolutePath="$(pwd)/$pathString"
+    fi
+
+    echo $absolutePath
+}
+
+
+# Backup the path to the current working directory
+cwdBackup="$(pwd)"
+
+# Argument $0 is the path to the script
+docsDirectoryPath="$(getAbsolutePath $(dirname $0))"
+sourceDirectoryPath="$docsDirectoryPath/.."
 
 # Generate the doxygen manual
-cd ..
+cd "$sourceDirectoryPath"
 doxygen
-make -C docs/doxygen/latex
+make -C "$docsDirectoryPath/doxygen/latex"
 
 # Extract the generated pdf
-cd docs
-mv doxygen/latex/refman.pdf .
-rm -r doxygen
+mv "$docsDirectoryPath/doxygen/latex/refman.pdf" "$docsDirectoryPath"
+rm -r "$docsDirectoryPath/doxygen"
 
 # Generate the documentation from the custom latex document
+cd "$docsDirectoryPath"
 pdflatex Dokumentation.tex
 
 # Remove the generated doxygen manual
-rm refman.pdf
+rm "$docsDirectoryPath/refman.pdf"
 
 # Remove every file that starts with "Dokumentation" except for Dokumentation.pdf and Dokumentation.tex
 rm $(find . -type f -name "Dokumentation*" ! -name "Dokumentation.pdf" ! -name "Dokumentation.tex")
+
+# Go back to the current working directory
+cd "$cwdBackup"
